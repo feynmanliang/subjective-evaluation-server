2a2cef9cedb17df679920a4fe363042621a3ce5f
Feynman Liang <feynman.liang@gmail.com>
2016-06-24 13:13:41 +0100

    Adds azure

---
+++ server.js
@@ -0,0 +1,7 @@
+import app from './src/app';
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Server listening on port ' + server.address().port);
+});
---
+++ src/app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan'
+import path from 'path';
+
+const app = express();
+const env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+    });
+});
+
+export default app;
