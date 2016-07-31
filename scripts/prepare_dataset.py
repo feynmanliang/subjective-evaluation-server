5bf7a16a8954363edd7fbc6c101200d713c38ccb
Feynman Liang <feynman.liang@gmail.com>
2016-07-31 23:09:03 +0100

    Updates prepare dataset to use urls

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -10,15 +10,20 @@ import json
 from glob import glob
 from os import path
 
-#EXPERIMENTS_CONTAINER_URL = "https://bachbot.blob.core.windows.net/experiments"
-EXPERIMENTS_CONTAINER_URL = "."
+EXPERIMENTS_CONTAINER_URL = "https://bachbot.blob.core.windows.net/experiments"
+#EXPERIMENTS_CONTAINER_URL = "."
 
 experimentName = path.basename(path.dirname(path.abspath(__file__))) # current dir
 
 experiment = dict()
 experiment['id'] = experimentName
 experiment['questions'] = json.load(open('questions.json', 'r'))
+for _, group in experiment['questions'].items():
+    for q in group:
+        for k, fname in q.items():
+            q[k] = path.join(EXPERIMENTS_CONTAINER_URL, experimentName, fname)
 
 with open('experiment.json', 'w') as fd:
+    print json.dumps(experiment, indent=4)
     json.dump(experiment, fd)
 
