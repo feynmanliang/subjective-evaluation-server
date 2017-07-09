197a19c4e859d0bbec76f2e0f3a2313f002870b1
Feynman Liang <feynman.liang@gmail.com>
2017-07-09 16:18:20 -0700

    Fix download syntax

--- scripts/download_all_responses.zsh
+++ scripts/download_all_responses.zsh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
 
-lines=("${(@f)$(az storage blob list responses \
+lines=("${(@f)$(az storage blob list -c responses \
   | grep 8-1-bachbot-questions)}");
 print $#lines
 for line in $lines; do
   item=$(echo $line | awk '{ print $2 }')
   if [[ ! -e $item ]]; then
-    az storage blob download responses $item
+    az storage blob download -c responses -n $item -f $item
   fi
 done
