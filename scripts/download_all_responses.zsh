02cb7b7dbeeccc0dce8c98bcd7fc92038de56cc3
Feynman Liang <feynman.liang@gmail.com>
2017-07-09 16:29:10 -0700

    Fix download script

--- scripts/download_all_responses.zsh
+++ scripts/download_all_responses.zsh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
 
 
-lines=("${(@f)$(az storage blob list -c responses \
-  | grep 8-1-bachbot-questions)}");
-print $#lines
-for line in $lines; do
-  item=$(echo $line | awk '{ print $2 }')
+lines=("${(@f)$(az storage blob list -c responses | jq -r '.[] | .name' | grep 8-1-bachbot-questions)}")
+for item in $lines; do
   if [[ ! -e $item ]]; then
+    touch $item
     az storage blob download -c responses -n $item -f $item
   fi
 done
